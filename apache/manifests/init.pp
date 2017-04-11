68861c42a6085db731a3c9769e139fad163e9e0a
Niko Kaartinen <netkaartinen@gmail.com>
2017-04-11 15:24:37 +0300

    Make public_html the default page for localhost

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -1,33 +1,59 @@
 class apache {
+
 	package {'apache2':
 		ensure => "installed",
 	}
+
 	service {'apache2':
 		ensure => "running",
 		enable => "true",
 		require => Package["apache2"],
 	}
+
 	file {'/etc/apache2/mods-enabled/userdir.load':
 		ensure => "link",
 		target => "/etc/apache2/mods-available/userdir.load",
 		notify => Service["apache2"],
 		require => Package["apache2"],
 	}
+
 	file {'/etc/apache2/mods-enabled/userdir.conf':
 		ensure => "link",
 		target => "/etc/apache2/mods-available/userdir.conf",
 		notify => Service["apache2"],
 		require => Package["apache2"],
 	}
+
 	file {'/home/niko/public_html':
 		ensure => "directory",
 		owner => "niko",
 		group => "niko",
 	}
+
 	file {'/home/niko/public_html/index.html':
-		content => template("apache/index_config"),
+		content => template("apache/html.template"),
 		owner => "niko",
 		group => "niko",
+	}
+
+	file {'/etc/apache2/sites-available/niko.conf':
+                content => template("apache/niko.conf.template"),
+                require => Package["apache2"],
+		owner => "root",
+                group => "root",
+        }
+	
+	file {'/etc/apache2/sites-enabled/niko.conf':
+		ensure => "link",
+		target => "/etc/apache2/sites-available/niko.conf",
+		notify => Service["apache2"],
+                require => Package["apache2"],
+	}
+	
+	file {'/etc/apache2/sites-enabled/000-default.conf':
+		ensure => "absent",
+		notify => Service["apache2"],
+		require => Package["apache2"],
 	}
 }
 
