59608825c3f96ae1f90a9204201935b116aa9f78
Niko Kaartinen <netkaartinen@gmail.com>
2017-05-03 09:04:48 +0300

    Add working init.pp for curl

--- curl/manifests/init.pp
+++ curl/manifests/init.pp
@@ -1,5 +1,11 @@
 class curl {
-	exec {"apt-get update":
+
+	exec {'apt-get update':
 		path => ["/usr/bin"],
+	}
+
+	package {'curl':
+		ensure => "installed",
+		require => Exec["apt-get update"],
 	}
 }
